5fc26871440e20fa5fd975d179c462d6b284bcb1
Rich Harris <richard.a.harris@gmail.com>
2017-09-04 23:22:55 -0400

    run prettier

--- src/index.js
+++ src/index.js
@@ -14,55 +14,59 @@ function longest(a, b) {
 	return b.length - a.length;
 }
 
-export default function replace ( options = {} ) {
-	const filter = createFilter( options.include, options.exclude );
-	const delimiters = ( options.delimiters || [ '', '' ] ).map( escape );
+export default function replace(options = {}) {
+	const filter = createFilter(options.include, options.exclude);
+	const delimiters = (options.delimiters || ['', '']).map(escape);
 
 	let values;
 
-	if ( options.values ) {
+	if (options.values) {
 		values = options.values;
 	} else {
-		values = Object.assign( {}, options );
+		values = Object.assign({}, options);
 		delete values.delimiters;
 		delete values.include;
 		delete values.exclude;
 	}
 
-	const keys = Object.keys( values ).sort( longest ).map(escape);
-	const pattern = new RegExp( delimiters[0] + '(' + keys.join( '|' ) + ')' + delimiters[1], 'g' );
+	const keys = Object.keys(values).sort(longest).map(escape);
+	const pattern = new RegExp(
+		delimiters[0] + '(' + keys.join('|') + ')' + delimiters[1],
+		'g'
+	);
 
 	// convert all values to functions
-	Object.keys( values ).forEach( key => {
-		values[ key ] = functor( values[ key ] );
+	Object.keys(values).forEach(key => {
+		values[key] = functor(values[key]);
 	});
 
 	return {
 		name: 'replace',
 
-		transform ( code, id ) {
-			if ( !filter( id ) ) return null;
+		transform(code, id) {
+			if (!filter(id)) return null;
 
-			const magicString = new MagicString( code );
+			const magicString = new MagicString(code);
 
 			let hasReplacements = false;
 			let match;
 			let start, end, replacement;
 
-			while ( match = pattern.exec( code ) ) {
+			while ((match = pattern.exec(code))) {
 				hasReplacements = true;
 
 				start = match.index;
 				end = start + match[0].length;
-				replacement = String( values[ match[1] ]( id ) );
+				replacement = String(values[match[1]](id));
 
-				magicString.overwrite( start, end, replacement );
+				magicString.overwrite(start, end, replacement);
 			}
 
-			if ( !hasReplacements ) return null;
+			if (!hasReplacements) return null;
 
 			let result = { code: magicString.toString() };
-			if ( options.sourceMap !== false ) result.map = magicString.generateMap({ hires: true });
+			if (options.sourceMap !== false)
+				result.map = magicString.generateMap({ hires: true });
 
 			return result;
 		}
