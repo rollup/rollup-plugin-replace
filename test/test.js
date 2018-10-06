0b62100bce3ebdf5c0efa231cf7973894a17db09
Arnaud Ceccaldi <caccialdo@gmail.com>
2018-10-06 07:39:48 +0100

    FIX: Stop mutating values option (#22)
    * FIX: Stop mutating values option
    
    * Add regression unit test
    
    * lint
    
    * Safety first

--- test/test.js
+++ test/test.js
@@ -36,6 +36,19 @@ describe('rollup-plugin-replace', () => {
 		assert.equal(code.trim(), 'console.log(42);');
 	});
 
+	it('does not mutate the values map properties', async () => {
+		const valuesMap = { ANSWER: '42' }
+		const bundle = await rollup({
+			input: 'samples/basic/main.js',
+			plugins: [
+				replace({ values: valuesMap })
+			]
+		});
+
+		await bundle.generate({ format: 'es' });
+		assert.equal(valuesMap.ANSWER, '42');
+	});
+
 	it('allows replacement to be a function', async () => {
 		const bundle = await rollup({
 			input: 'samples/relative/main.js',
